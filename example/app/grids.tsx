c27cb76255a40f41e2674bb827711c157fbf8f73
oliverloops <oliveralr@protonmail.com>
2025-01-13 02:06:01 -0600

    Grids screen examples added

--- example/app/grids.tsx
+++ example/app/grids.tsx
@@ -5,7 +5,35 @@ export default function GridsExample() {
   return (
     <ScrollView style={{ flex: 1 }} contentContainerStyle={{ padding: 20 }}>
       <Text style={styles.header}>Grids Example</Text>
+      <Text style={[styles.header, { fontSize: 24 }]}>LazyVerticalGrid</Text>
+      {/* vertical grid it's by default */}
       <Grid gridCellsType="fixed" size={5}>
+        {Array.from({ length: 50 }).map((_, index) => (
+          <Text key={index}>{index}</Text>
+        ))}
+      </Grid>
+
+      <Text style={[styles.header, { fontSize: 24 }]}>
+        LazyVerticalStaggeredGrid
+      </Text>
+      {/* vertical staggered grid it's by default */}
+      <Grid staggered spacedBy={5}>
+        {Array.from({ length: 30 }).map((_, index) => (
+          <Text key={index}>{index}</Text>
+        ))}
+      </Grid>
+
+      <Text style={[styles.header, { fontSize: 24 }]}>LazyHorizontalGrid</Text>
+      <Grid horizontal gridCellsType="adaptive">
+        {Array.from({ length: 30 }).map((_, index) => (
+          <Text key={index}>{index}</Text>
+        ))}
+      </Grid>
+
+      <Text style={[styles.header, { fontSize: 24 }]}>
+        LazyHorizontalStaggeredGrid
+      </Text>
+      <Grid horizontal staggered spacedBy={5} style={{ height: 150 }}>
         {Array.from({ length: 30 }).map((_, index) => (
           <Text key={index}>{index}</Text>
         ))}
